4bd58c297d4346b536f59b2f7345d6e736c84b3e
Steven de Oliveira <de.oliveira.steven@gmail.com>
2026-08-13 10:16:44 +0200

    Clarification du script de build

--- scripts/lazy_compile/build.sh
+++ scripts/lazy_compile/build.sh
@@ -1,1 +1,8 @@
-opam exec -- ocamlfind ocamlc -package str -package unix -package cmdliner -linkpkg -o lcc utils.ml utils.mli dep_graph.mli dep_graph.ml cli.mli cli.ml main.ml
+opam exec -- ocamlfind ocamlc \
+     -package str -package unix -package cmdliner \
+     -linkpkg \
+     -o lcc \
+     utils.mli utils.ml \
+     dep_graph.mli dep_graph.ml \
+     cli.mli cli.ml \
+     main.ml
